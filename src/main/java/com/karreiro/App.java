f28f7363d3752380004de12cdad71e884f2816db
Guilherme Carreiro <karreiro@gmail.com>
2017-06-10 18:01:31 -0300

    Initial commit

---
+++ src/main/java/com/karreiro/App.java
@@ -0,0 +1,13 @@
+package com.karreiro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
